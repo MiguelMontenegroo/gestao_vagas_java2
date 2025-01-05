1ae718a7ff5e460cf38a93cd7565736ee43c079e
MiguelMontenegroo <miguellaticiniosmontenegro@gmail.com>
2025-01-05 02:09:05 -0300

    swagger documentation

--- src/main/java/br/com/projeto2/gestao_vagas/modules/company/dto/CreateJobDTO.java
+++ src/main/java/br/com/projeto2/gestao_vagas/modules/company/dto/CreateJobDTO.java
@@ -1,12 +1,18 @@
 package br.com.projeto2.gestao_vagas.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.Data;
 
 @Data
 public class CreateJobDTO {
   
-
+  @Schema(example = "Vaga para pessoa desenvolvedora júnior", requiredMode = RequiredMode.REQUIRED)
   private String description;
+
+  @Schema(example = "Gympass, plano de saúde", requiredMode = RequiredMode.REQUIRED)
   private String benefits;
+
+  @Schema(example = "JUNIOR", requiredMode = RequiredMode.REQUIRED)
   private String level;
 }
